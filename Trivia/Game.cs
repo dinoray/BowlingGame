7cce19badfb530b97550b044034ae8180ff7d663
dino232003 <dino232003@hotmail.com>
2017-11-03 17:44:32 +0800

    add BowlingDino Project
    finish Strike

---
+++ Trivia/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingDino
+{
+    public class Game
+    {
+        private int bottle;
+        private int round;
+        private string result;
+        private string gameState;
+
+        public Game()
+        {
+            bottle = 10;
+            round = 1;
+            result = "";
+        }
+
+        public void Hit(int hitAmount)
+        {
+            bottle -= hitAmount;
+
+            if (bottle == 0 && round == 1)
+            {
+                result = "Strike";
+            }
+        }
+
+        public string getResult()
+        {
+            return result;
+        }
+    }
+
+}
---
+++ Trivia/GameTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+
+namespace BowlingDino
+{
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    class GameTest
+    {
+        [Test]
+        public void Strike()
+        {
+            var game = new Game();
+
+            game.Hit(10);
+            
+            Assert.Equals(game.getResult(),"Strike");
+        }
+    }
+}
